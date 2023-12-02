43566bd8de4a1b85a06f97408d56b300d728f55c
Francine Guzman <francineguzman10@gmail.com>
2023-12-02 20:27:07 +0800

    Initial commit

---
+++ app/src/main/java/com/labactivity/gakumate/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.labactivity.gakumate
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.labactivity.gakumate.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
+    }
+
+}
---
+++ app/src/main/java/com/labactivity/gakumate/SplashScreenActivity.kt
@@ -0,0 +1,26 @@
+package com.labactivity.gakumate
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.labactivity.gakumate.databinding.ActivitySplashScreenBinding
+
+class SplashScreenActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashScreenBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val imgView_splash = binding.splashImage
+
+        imgView_splash.alpha =0f
+        imgView_splash.animate().setDuration(1500).alpha(1f).withEndAction{
+            val i = Intent(this,MainActivity::class.java)
+            startActivity(i)
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+            finish()
+        }
+
+    }
+}
