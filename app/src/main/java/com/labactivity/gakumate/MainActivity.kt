e697ec678ebd0d67f2e84fd1158f2f2510651d07
Francine Guzman <francineguzman10@gmail.com>
2023-12-03 18:28:04 +0800

    connected main activity to dialog plus

--- app/src/main/java/com/labactivity/gakumate/MainActivity.kt
+++ app/src/main/java/com/labactivity/gakumate/MainActivity.kt
@@ -3,6 +3,8 @@ package com.labactivity.gakumate
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.labactivity.gakumate.databinding.ActivityMainBinding
+import com.orhanobut.dialogplus.DialogPlus
+import com.orhanobut.dialogplus.ViewHolder
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -10,6 +12,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val add = binding.btnAdd
+
+        add.setOnClickListener{
+            val dialogBuilder = DialogPlus.newDialog(this)
+                .setContentHolder(ViewHolder(R.layout.show_add))
+                .setExpanded(false) // This is optional, set it as per your requirement
+                .create()
+
+            // Show the dialog
+            dialogBuilder.show()        }
 
 
 
