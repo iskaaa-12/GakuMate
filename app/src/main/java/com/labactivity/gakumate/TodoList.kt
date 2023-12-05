14629fcbb3d5289771afb08088b36b4341ee4fb1
Cheeska Mae Yabes <maecheeskamae@gmail.com>
2023-12-05 15:37:39 +0800

    todo list

--- app/src/main/java/com/labactivity/gakumate/TodoList.kt
+++ app/src/main/java/com/labactivity/gakumate/TodoList.kt
@@ -3,18 +3,54 @@ package com.labactivity.gakumate
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.labactivity.gakumate.databinding.ActivityTodoListBinding
+import com.labappdev.todolist.AddNotes
 
 class TodoList : AppCompatActivity() {
     private lateinit var binding: ActivityTodoListBinding
+    private val tasksList = ArrayList<Tasks>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityTodoListBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.floatingAddRecBtn.setOnClickListener(){
+
+        val layoutManager = LinearLayoutManager(this)
+        binding.recyclerViewTasks.layoutManager = layoutManager
+
+        val adapter = CategoryAdapter(tasksList)
+        binding.recyclerViewTasks.adapter = adapter
+
+
+        binding.floatingAddRecBtn.setOnClickListener {
             val intentAddNotes = Intent(this, AddNotes::class.java)
-            startActivity(intentAddNotes)
+            startActivityForResult(intentAddNotes, ADD_NOTES_REQUEST)
         }
+
+        binding.imgBtnBack.setOnClickListener(){
+            finish()
+        }
+
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == ADD_NOTES_REQUEST && resultCode == RESULT_OK) {
+            val newTask = data?.getSerializableExtra("newTask") as? Tasks
+            if (newTask != null) {
+                tasksList.add(newTask)
+
+                tasksList.sortBy { it.date }
+
+                (binding.recyclerViewTasks.adapter as? CategoryAdapter)?.notifyDataSetChanged()
+            }
+        }
+    }
+
+    companion object {
+        const val ADD_NOTES_REQUEST = 1
     }
 }
